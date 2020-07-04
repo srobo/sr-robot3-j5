1c860c3a357f720530f1a28c8b6c75c4694573c0
Dan Trickey <dan@trickey.io>
2020-07-04 19:07:00 +0100

    Expose some constants

--- sr/robot3/__init__.py
+++ sr/robot3/__init__.py
@@ -1,11 +1,20 @@
 """sr.robot3 - Student Robotics API for Python 3."""
 
+from j5.components.motor import MotorSpecialState
+from j5.components.piezo import Note
+
 from .logging import logger_setup
 from .robot import Robot, __version__
 
 logger_setup()
 
+COAST = MotorSpecialState.COAST
+BRAKE = MotorSpecialState.BRAKE
+
 __all__ = [
     "__version__",
+    "BRAKE",
+    "COAST",
+    "Note",
     "Robot",
 ]
