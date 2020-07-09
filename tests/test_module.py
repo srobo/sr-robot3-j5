c66631163f04883755fdf3c4938efce50457b41b
Dan Trickey <dan@trickey.io>
2020-07-09 19:17:42 +0100

    Test that the power output constants are exported

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,5 +1,7 @@
 """Test that the sr.robot3 imports as expected."""
+
 import sr.robot3
+from j5.boards.sr.v4 import PowerOutputPosition
 from packaging.version import Version
 
 
@@ -9,3 +11,12 @@ def test_module_version() -> None:
 
     version = Version(sr.robot3.__version__)
     assert str(version) == sr.robot3.__version__
+
+
+def test_power_outputs_exported() -> None:
+    """Test that the power output constants are exported."""
+    EXPORT_PREFIX = "OUT_"
+
+    for pos in PowerOutputPosition:
+        pos_from_module = getattr(sr.robot3, EXPORT_PREFIX + pos.name)
+        assert pos_from_module is pos
