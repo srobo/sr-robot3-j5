14ac5761b57173a9d88f7663164b2573913a66e1
Dan Trickey <dan@trickey.io>
2020-07-09 19:11:22 +0100

    Expose PowerOutputPosition constants

--- sr/robot3/__init__.py
+++ sr/robot3/__init__.py
@@ -1,5 +1,6 @@
 """sr.robot3 - Student Robotics API for Python 3."""
 
+from j5.boards.sr.v4 import PowerOutputPosition
 from j5.components.motor import MotorSpecialState
 from j5.components.piezo import Note
 
@@ -11,10 +12,23 @@ logger_setup()
 COAST = MotorSpecialState.COAST
 BRAKE = MotorSpecialState.BRAKE
 
+OUT_H0 = PowerOutputPosition.H0
+OUT_H1 = PowerOutputPosition.H1
+OUT_L0 = PowerOutputPosition.L0
+OUT_L1 = PowerOutputPosition.L1
+OUT_L2 = PowerOutputPosition.L2
+OUT_L3 = PowerOutputPosition.L3
+
 __all__ = [
     "__version__",
     "BRAKE",
     "COAST",
+    "OUT_H0",
+    "OUT_H1",
+    "OUT_L0",
+    "OUT_L1",
+    "OUT_L2",
+    "OUT_L3",
     "Note",
     "Robot",
 ]
