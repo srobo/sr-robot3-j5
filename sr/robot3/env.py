d1458ccf5d0e039a973d9a1fd1cd0e546b8d07c8
Dan Trickey <dan@trickey.io>
2020-07-30 20:43:22 +0100

    Register Ruggeduino Hardware Backend

--- sr/robot3/env.py
+++ sr/robot3/env.py
@@ -10,6 +10,7 @@ from j5.backends.hardware.sr.v4 import (
     SRV4MotorBoardHardwareBackend,
     SRV4PowerBoardHardwareBackend,
     SRV4ServoBoardHardwareBackend,
+    SRV4RuggeduinoHardwareBackend
 )
 
 __all__ = [
@@ -22,6 +23,7 @@ HARDWARE_ENVIRONMENT = Environment("Hardware Environment")
 HARDWARE_ENVIRONMENT.register_backend(SRV4PowerBoardHardwareBackend)
 HARDWARE_ENVIRONMENT.register_backend(SRV4ServoBoardHardwareBackend)
 HARDWARE_ENVIRONMENT.register_backend(SRV4MotorBoardHardwareBackend)
+HARDWARE_ENVIRONMENT.register_backend(SRV4RuggeduinoHardwareBackend)
 
 CONSOLE_ENVIRONMENT = Environment("Console Environment")
 
