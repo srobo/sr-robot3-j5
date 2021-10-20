569d8f9c17ed4e8ec69afb310e041236a2df3e73
Karina Kwiatek <kjk@kjk.dog>
2021-10-20 18:17:26 +0200

    game: Rename MARKER_SIZE_LUT  to MARKER_SIZES
    Co-authored-by: Jake Howard <RealOrangeOne@users.noreply.github.com>

--- sr/robot3/game.py
+++ sr/robot3/game.py
@@ -6,7 +6,7 @@ class UnusedMarkerException(Exception):
     """This marker is not used for the game."""
 
 
-MARKER_SIZE_LUT: Dict[Container[int], int] = {
+MARKER_SIZES: Dict[Container[int], int] = {
     range(28): 250,  # 0 - 27 for arena boundary
 }
 
@@ -18,7 +18,7 @@ def marker_used_in_game(marker_id: int) -> int:
     :param marker_id: An official marker number, mapped to the competitor range.
     :returns: True if the market is used in the game.
     """
-    return any([marker_id in marker_range for marker_range in MARKER_SIZE_LUT])
+    return any([marker_id in marker_range for marker_range in MARKER_SIZES])
 
 
 def get_marker_size(marker_id: int) -> int:
@@ -28,7 +28,7 @@ def get_marker_size(marker_id: int) -> int:
     :param marker_id: An official marker number, mapped to the competitor range.
     :returns: Size of the marker in millimetres.
     """
-    for marker_range, size in MARKER_SIZE_LUT.items():
+    for marker_range, size in MARKER_SIZES.items():
         if marker_id in marker_range:
             return size
     raise UnusedMarkerException(f"{marker_id} is not used for the game.")
