c5d5527d677caedbfb3f315790f18c6cfe7ed3ee
Karina Kwiatek <kjk@kjk.dog>
2021-10-24 15:51:53 +0200

    env: Rename `VISION_ENVIRONMENT` to `CONSOLE_ENVIRONMENT_WITH_VISION`
    Co-authored-by: Jake Howard <RealOrangeOne@users.noreply.github.com>

--- examples/vision.py
+++ examples/vision.py
@@ -1,7 +1,7 @@
 from sr.robot3 import Robot
-from sr.robot3.env import VISION_ONLY_ENVIRONMENT
+from sr.robot3.env import CONSOLE_ENVIRONMENT_WITH_VISION
 
-R = Robot(env=VISION_ONLY_ENVIRONMENT, auto_start=False)
+R = Robot(env=CONSOLE_ENVIRONMENT_WITH_VISION, auto_start=False)
 
 if __name__ == '__main__':
     while True:
