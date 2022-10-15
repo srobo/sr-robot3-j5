239e74427faf1ebc19eb821c391cb9cb0843558f
WillB97 <will@rudi.org.uk>
2022-10-15 01:07:34 +0100

    Bump version to 2023.0.2

--- sr/robot3/robot.py
+++ sr/robot3/robot.py
@@ -24,7 +24,7 @@ from .kch import KCH
 from .timeout import kill_after_delay
 from .vision import SRZolotoHardwareBackend
 
-__version__ = "2023.0.1"
+__version__ = "2023.0.2"
 
 
 LOGGER = logging.getLogger(__name__)
